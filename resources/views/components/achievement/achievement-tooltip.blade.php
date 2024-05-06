6172f4f10e1a37d0b511af5ad5b5298b139705f2
Shirloin <110618626+Shirloin@users.noreply.github.com>
2024-05-06 22:45:32 +0700

    fix(ui): profile tooltip margin

--- resources/views/components/achievement/achievement-tooltip.blade.php
+++ resources/views/components/achievement/achievement-tooltip.blade.php
@@ -1,6 +1,6 @@
 <div class="relative inline-block" x-data="{ tooltip: false }">
     <div x-show="tooltip"
-        class="w-60 absolute flex flex-col bg-panel-1000 text-white text-sm rounded py-2 px-4 mb-1 -translate-y-full">
+        class="w-56 absolute flex flex-col bg-panel-1000 text-white text-sm rounded-xl py-2 px-4 -translate-y-full -translate-x-3 -top-3">
         <span class="font-bold mb-2">{{ $achievement->name }}</span>
         <span class="text-xs font-medium">{{ $achievement->description }}</span>
     </div>
