b1f7c2cd45b0b519f6d6d5a98961fe5a07b48e19
Riccardo <riccardo@binus.ac.id>
2024-05-21 14:34:05 +0700

    feat: add logo

--- resources/views/components/layouts/template.blade.php
+++ resources/views/components/layouts/template.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CodeXchange</title>
+    <link rel="icon" type="image/x-icon" href="/assets/logo.png">
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
